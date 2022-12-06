9fe8c1e252d91d64a24f89118da4f376edd8f6ca
thnDiep <thndiepkhtn@gmail.com>
2022-12-07 00:21:00 +0700

    install and config lint-staged, husky, prettier

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-import express from "express";
-import { engine } from "express-handlebars";
-import morgan from "morgan";
-import route from "./routes/index.js";
-import hbs_sections from "express-handlebars-sections";
+import express from 'express';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import route from './routes/index.js';
+import hbs_sections from 'express-handlebars-sections';
 const app = express();
 
 // Logger HTTP
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 
 // static file in public folder
-app.use("/public", express.static("public"));
+app.use('/public', express.static('public'));
 
 // Config handlebars
 app.engine(
-  "hbs",
-  engine({
-    extname: "hbs",
-    helpers: {
-      section(name, options) {
-        if (!this._sections) {
-          this._sections = {};
-        }
-        this._sections[name] = options.fn(this);
-        return null;
-      },
-    },
-  })
+    'hbs',
+    engine({
+        extname: 'hbs',
+        helpers: {
+            section(name, options) {
+                if (!this._sections) {
+                    this._sections = {};
+                }
+                this._sections[name] = options.fn(this);
+                return null;
+            },
+        },
+    }),
 );
 
-app.set("view engine", "hbs");
-app.set("views", "./views");
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
 route(app);
 
 app.listen(3000, () => {
-  console.log("Listening: http://localhost:3000");
+    console.log('Listening: http://localhost:3000');
 });
