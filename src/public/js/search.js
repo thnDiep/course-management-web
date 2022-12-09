ea9dae8cac0c86049fe48d4b72fe927cede5058e
Manh Tu <manhtu2272002@gmail.com>
2022-12-09 13:26:12 +0700

    fix UI header, sidebar reponsive create event

---
+++ src/public/js/search.js
@@ -0,0 +1,20 @@
+const sidebar_button = document.querySelector('.sidebar1__search');
+const overlay = document.querySelector('.overlay')
+
+sidebar_button.addEventListener('click', function () {
+    const overlay = document.querySelector('.overlay')
+
+    if (overlay !== null) {
+        overlay.remove();
+        document.querySelector('.header').classList.toggle("hidden");
+    }
+    else {
+        document.querySelector('.header').classList.toggle("hidden");
+        const html = '<div onclick="myFunction()" class="overlay"></div>';
+        document.querySelector('.container-fluid').insertAdjacentHTML('beforebegin', html)
+    }
+})
+function myFunction() {
+    document.querySelector('.overlay').remove();
+    document.querySelector('.header').classList.toggle("hidden");
+}
