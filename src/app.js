c1c892417987a052db32336fe0e598520819c4ef
Vylee <87347391+Vylee29@users.noreply.github.com>
2022-12-09 20:28:38 +0700

    done ui login

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
-import morgan from 'morgan';
-import route from './routes/index.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import hbs_sections from 'express-handlebars-sections';
+import express from "express";
+import { engine } from "express-handlebars";
+import morgan from "morgan";
+import route from "./routes/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import hbs_sections from "express-handlebars-sections";
 
 // get __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -13,35 +13,35 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Logger HTTP
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 
 // Static file in public folder
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 // Config handlebars
 // Config handlebars
 app.engine(
-    'hbs',
-    engine({
-        extname: 'hbs',
-        helpers: {
-            section(name, options) {
-                if (!this._sections) {
-                    this._sections = {};
-                }
-                this._sections[name] = options.fn(this);
-                return null;
-            },
-        },
-    }),
+  "hbs",
+  engine({
+    extname: "hbs",
+    helpers: {
+      section(name, options) {
+        if (!this._sections) {
+          this._sections = {};
+        }
+        this._sections[name] = options.fn(this);
+        return null;
+      },
+    },
+  })
 );
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 route(app);
 
 app.listen(3000, () => {
-    console.log('Listening: http://localhost:3000');
+  console.log("Listening: http://localhost:3000");
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,31 +4,31 @@ export default function route(app) {
   // Routes
   app.get("/login", (req, res) => {
     res.render("login", {
-      check: false
+      check: false,
     });
   });
 
   app.get("/signup", (req, res) => {
     res.render("signUp", {
-      check: false
+      check: false,
     });
   });
 
   app.get("/", (req, res) => {
     res.render("home", {
-      check: true
+      check: true,
     });
   });
 
   app.get("/profile", (req, res) => {
     res.render("profile", {
-      check: false
+      check: false,
     });
   });
 
   app.get("/courses", (req, res) => {
     res.render("courses", {
-      check: false
+      check: false,
     });
   });
 
@@ -36,7 +36,7 @@ export default function route(app) {
 
   app.get("/courses/courseDetail", (req, res) => {
     res.render("courseDetail", {
-      check: false
+      check: false,
     });
   });
 }
