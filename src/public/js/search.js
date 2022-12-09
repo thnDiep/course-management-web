669491a2b5cabdf49bf17f46e89c2fec0b41193f
Manh Tu <manhtu2272002@gmail.com>
2022-12-09 16:42:14 +0700

    Design sidebar--icon when move page

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -6,15 +6,15 @@ sidebar_button.addEventListener('click', function () {
 
     if (overlay !== null) {
         overlay.remove();
-        document.querySelector('.header').classList.toggle("hidden");
+        document.querySelector('.header').classList.toggle("show");
     }
     else {
-        document.querySelector('.header').classList.toggle("hidden");
+        document.querySelector('.header').classList.toggle("show");
         const html = '<div onclick="myFunction()" class="overlay"></div>';
         document.querySelector('.container-fluid').insertAdjacentHTML('beforebegin', html)
     }
 })
 function myFunction() {
     document.querySelector('.overlay').remove();
-    document.querySelector('.header').classList.toggle("hidden");
+    document.querySelector('.header').classList.toggle("show");
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,28 +3,40 @@ import searchRoute from "./search.route.js";
 export default function route(app) {
   // Routes
   app.get("/login", (req, res) => {
-    res.render("login");
+    res.render("login", {
+      check: false
+    });
   });
 
   app.get("/signup", (req, res) => {
-    res.render("signUp");
+    res.render("signUp", {
+      check: false
+    });
   });
 
   app.get("/", (req, res) => {
-    res.render("home");
+    res.render("home", {
+      check: true
+    });
   });
 
   app.get("/profile", (req, res) => {
-    res.render("profile");
+    res.render("profile", {
+      check: false
+    });
   });
 
   app.get("/courses", (req, res) => {
-    res.render("courses");
+    res.render("courses", {
+      check: false
+    });
   });
 
   app.use("/search", searchRoute);
 
-  app.get("/courseDetail", (req, res) => {
-    res.render("courseDetail");
+  app.get("/courses/courseDetail", (req, res) => {
+    res.render("courseDetail", {
+      check: false
+    });
   });
 }
