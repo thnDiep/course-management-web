603af1db727c9eed6c48f6828f190a414c26b987
Tran-thaoquyen23 <92144715+Tran-thaoquyen23@users.noreply.github.com>
2022-12-19 05:53:06 +0700

    update db

--- src/utils/academydb.sql
+++ src/utils/academydb.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 17, 2022 at 05:26 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Máy chủ: localhost:3307
+-- Thời gian đã tạo: Th12 18, 2022 lúc 11:48 PM
+-- Phiên bản máy phục vụ: 10.4.25-MariaDB
+-- Phiên bản PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,23 +18,23 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `academydb`
+-- Cơ sở dữ liệu: `academydb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Cấu trúc bảng cho bảng `category`
 --
 
 CREATE TABLE `category` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(250) NOT NULL,
   `parentID` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `category`
+-- Đang đổ dữ liệu cho bảng `category`
 --
 
 INSERT INTO `category` (`id`, `name`, `parentID`) VALUES
@@ -58,17 +58,17 @@ INSERT INTO `category` (`id`, `name`, `parentID`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chapter`
+-- Cấu trúc bảng cho bảng `chapter`
 --
 
 CREATE TABLE `chapter` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(250) NOT NULL,
+  `name` varchar(250) COLLATE utf8_bin NOT NULL,
   `courseID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `chapter`
+-- Đang đổ dữ liệu cho bảng `chapter`
 --
 
 INSERT INTO `chapter` (`id`, `name`, `courseID`) VALUES
@@ -79,7 +79,7 @@ INSERT INTO `chapter` (`id`, `name`, `courseID`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `course`
+-- Cấu trúc bảng cho bảng `course`
 --
 
 CREATE TABLE `course` (
@@ -98,19 +98,19 @@ CREATE TABLE `course` (
   `benefit` text NOT NULL,
   `status` tinyint(4) NOT NULL,
   `views` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `course`
+-- Đang đổ dữ liệu cho bảng `course`
 --
 
 INSERT INTO `course` (`id`, `name`, `tinyDescription`, `fullDescription`, `fee`, `feeO`, `updateTime`, `image`, `videoID`, `rated`, `idCategory`, `required`, `benefit`, `status`, `views`) VALUES
-(0000000001, 'Javascript for Beginners', 'Learn javascript online and supercharge your web design with this Javascript for beginners training course.', 'Take this Javascript training course and start learning Javascript today.\r\n\r\n\"As a business guy I have no place in programming.\" Ten years ago you could have gotten away with that statement. Today you say that to your colleagues and they scoff at you before they go back to their computers to fix real problems and do real work.\r\n\r\nIf you want to do something useful start by learning Javascript . In these days when the browser is central to all computer use knowing \"the language of the browser\" is the most important step. A few years ago Javascript potential was uncertain and many programmers considered it useless. These days however competent programmers have identified Javascript real potential and uses and it has gone from a toy language to the main language of the browser. It has become one of the most useful languages of this era. Every developer needs at least a basic understanding of Javascript. A developer who knows Javascript is the rockstar of the company and is in constant demand by employers. Our online Javascript\r\n\r\ncourse will get you started by teaching all the essential aspects of coding in Javascript. So... what\'s it gonna be? Do you want to supercharge your career and be in constant demand by employers? Do you want to learn how to create dynamic and innovative Javascript documents? Start programming today with our Javascript course for Beginners training and take control of your career.', 299000, 499000, '2022-12-13 16:38:53', 'https://i.ytimg.com/vi/vDQ9GZsJkms/maxresdefault.jpg', 'zBPeGR48_vE', '4.0', 2, 'Some basic knowledge of HTML is required for this course.', '<table class=\"table\">\r\n     <thead>\r\n            <tr>\r\n              <th colspan=\"2\">What will you learn</th>\r\n            </tr>\r\n          </thead>\r\n          <tbody>\r\n            <tr>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Extensive, informative and interesting video lecture\r\n              </td>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Complete Code demonstrated in lecture\r\n              </td>\r\n            </tr>\r\n            <tr>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Lab Exercises\r\n              </td>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Lab Solution Sets\r\n              </td>\r\n            </tr>\r\n            <tr>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                All Powerpoint Demonstrations Used in Course\r\n              </td>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Instructor contact Email for questions and clarifications\r\n              </td>\r\n            </tr>\r\n            <tr>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Coverage of all important primary Javascript concepts\r\n              </td>\r\n            </tr>\r\n          </tbody>\r\n        </table> ', 1, 12);
+(0000000001, 'Javascript for Beginners', 'Learn javascript online and supercharge your web design with this Javascript for beginners training course.', 'Take this Javascript training course and start learning Javascript today.\r\n\r\n\"As a business guy I have no place in programming.\" Ten years ago you could have gotten away with that statement. Today you say that to your colleagues and they scoff at you before they go back to their computers to fix real problems and do real work.\r\n\r\nIf you want to do something useful start by learning Javascript . In these days when the browser is central to all computer use knowing \"the language of the browser\" is the most important step. A few years ago Javascript potential was uncertain and many programmers considered it useless. These days however competent programmers have identified Javascript real potential and uses and it has gone from a toy language to the main language of the browser. It has become one of the most useful languages of this era. Every developer needs at least a basic understanding of Javascript. A developer who knows Javascript is the rockstar of the company and is in constant demand by employers. Our online Javascript\r\n\r\ncourse will get you started by teaching all the essential aspects of coding in Javascript. So... what\'s it gonna be? Do you want to supercharge your career and be in constant demand by employers? Do you want to learn how to create dynamic and innovative Javascript documents? Start programming today with our Javascript course for Beginners training and take control of your career.', 299000, 499000, '2022-12-13 16:38:53', 'https://securiumsolutions.com/blog/wp-content/uploads/2021/05/Javascript.png', 'zBPeGR48_vE', '4.0', 2, 'Some basic knowledge of HTML is required for this course.', '<table class=\"table\">\r\n     <thead>\r\n            <tr>\r\n              <th colspan=\"2\">What will you learn</th>\r\n            </tr>\r\n          </thead>\r\n          <tbody>\r\n            <tr>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Extensive, informative and interesting video lecture\r\n              </td>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Complete Code demonstrated in lecture\r\n              </td>\r\n            </tr>\r\n            <tr>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Lab Exercises\r\n              </td>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Lab Solution Sets\r\n              </td>\r\n            </tr>\r\n            <tr>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                All Powerpoint Demonstrations Used in Course\r\n              </td>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Instructor contact Email for questions and clarifications\r\n              </td>\r\n            </tr>\r\n            <tr>\r\n              <td>\r\n                <i class=\"fa fa-check\" aria-hidden=\"true\"></i>\r\n                Coverage of all important primary Javascript concepts\r\n              </td>\r\n            </tr>\r\n          </tbody>\r\n        </table> ', 1, 12);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `course_of_student`
+-- Cấu trúc bảng cho bảng `course_of_student`
 --
 
 CREATE TABLE `course_of_student` (
@@ -119,18 +119,10 @@ CREATE TABLE `course_of_student` (
   `chapterID` int(10) UNSIGNED DEFAULT NULL,
   `lessonID` int(10) UNSIGNED DEFAULT NULL,
   `time` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `course_of_student`
---
-
-INSERT INTO `course_of_student` (`studentID`, `courseID`, `chapterID`, `lessonID`, `time`) VALUES
-(1, 1, NULL, NULL, NULL),
-(2, 1, NULL, NULL, NULL);
-
---
--- Dumping data for table `course_of_student`
+-- Đang đổ dữ liệu cho bảng `course_of_student`
 --
 
 INSERT INTO `course_of_student` (`studentID`, `courseID`, `chapterID`, `lessonID`, `time`) VALUES
@@ -140,26 +132,16 @@ INSERT INTO `course_of_student` (`studentID`, `courseID`, `chapterID`, `lessonID
 -- --------------------------------------------------------
 
 --
--- Table structure for table `course_of_teacher`
+-- Cấu trúc bảng cho bảng `course_of_teacher`
 --
 
 CREATE TABLE `course_of_teacher` (
   `teacherID` int(10) UNSIGNED NOT NULL,
   `courseID` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `course_of_teacher`
---
-
-INSERT INTO `course_of_teacher` (`teacherID`, `courseID`) VALUES
-(1, 3),
-(1, 4),
-(3, 1),
-(3, 2);
-
---
--- Dumping data for table `course_of_teacher`
+-- Đang đổ dữ liệu cho bảng `course_of_teacher`
 --
 
 INSERT INTO `course_of_teacher` (`teacherID`, `courseID`) VALUES
@@ -171,7 +153,7 @@ INSERT INTO `course_of_teacher` (`teacherID`, `courseID`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `history_lesson`
+-- Cấu trúc bảng cho bảng `history_lesson`
 --
 
 CREATE TABLE `history_lesson` (
@@ -180,17 +162,10 @@ CREATE TABLE `history_lesson` (
   `time` time DEFAULT NULL,
   `courseID` int(11) NOT NULL,
   `studentID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `history_lesson`
---
-
-INSERT INTO `history_lesson` (`chapterID`, `lessonID`, `time`, `courseID`, `studentID`) VALUES
-(1, 2, '17:21:00', 1, 1);
-
---
--- Dumping data for table `history_lesson`
+-- Đang đổ dữ liệu cho bảng `history_lesson`
 --
 
 INSERT INTO `history_lesson` (`chapterID`, `lessonID`, `time`, `courseID`, `studentID`) VALUES
@@ -199,38 +174,28 @@ INSERT INTO `history_lesson` (`chapterID`, `lessonID`, `time`, `courseID`, `stud
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lesson`
+-- Cấu trúc bảng cho bảng `lesson`
 --
 
 CREATE TABLE `lesson` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(250) NOT NULL,
   `chapterID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `permission`
+-- Cấu trúc bảng cho bảng `permission`
 --
 
 CREATE TABLE `permission` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `permission`
---
-
-INSERT INTO `permission` (`id`, `name`) VALUES
-(1, 'admin'),
-(2, 'student'),
-(3, 'teacher'),
-(4, 'guest');
-
---
--- Dumping data for table `permission`
+-- Đang đổ dữ liệu cho bảng `permission`
 --
 
 INSERT INTO `permission` (`id`, `name`) VALUES
@@ -242,7 +207,7 @@ INSERT INTO `permission` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rating`
+-- Cấu trúc bảng cho bảng `rating`
 --
 
 CREATE TABLE `rating` (
@@ -252,19 +217,10 @@ CREATE TABLE `rating` (
   `courseID` int(11) NOT NULL,
   `studentID` int(11) NOT NULL,
   `time` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `rating`
---
-
-INSERT INTO `rating` (`id`, `star`, `feedback`, `courseID`, `studentID`, `time`) VALUES
-(1, 2, 'hello', 2, 1, '2022-12-16 08:33:51'),
-(2, 5, 'hekko', 1, 2, '2022-12-15 08:42:43'),
-(3, 3, 'fasfas', 1, 3, '2022-12-14 13:42:43');
-
---
--- Dumping data for table `rating`
+-- Đang đổ dữ liệu cho bảng `rating`
 --
 
 INSERT INTO `rating` (`id`, `star`, `feedback`, `courseID`, `studentID`, `time`) VALUES
@@ -275,24 +231,16 @@ INSERT INTO `rating` (`id`, `star`, `feedback`, `courseID`, `studentID`, `time`)
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student_love_course`
+-- Cấu trúc bảng cho bảng `student_love_course`
 --
 
 CREATE TABLE `student_love_course` (
   `studentID` int(10) UNSIGNED NOT NULL,
   `courseID` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `student_love_course`
---
-
-INSERT INTO `student_love_course` (`studentID`, `courseID`) VALUES
-(1, 1),
-(2, 1);
-
---
--- Dumping data for table `student_love_course`
+-- Đang đổ dữ liệu cho bảng `student_love_course`
 --
 
 INSERT INTO `student_love_course` (`studentID`, `courseID`) VALUES
@@ -302,7 +250,7 @@ INSERT INTO `student_love_course` (`studentID`, `courseID`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Cấu trúc bảng cho bảng `user`
 --
 
 CREATE TABLE `user` (
@@ -312,10 +260,10 @@ CREATE TABLE `user` (
   `email` varchar(50) NOT NULL,
   `permissionID` int(10) UNSIGNED NOT NULL,
   `about` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `user`
+-- Đang đổ dữ liệu cho bảng `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `password`, `email`, `permissionID`, `about`) VALUES
@@ -324,11 +272,11 @@ INSERT INTO `user` (`id`, `name`, `password`, `email`, `permissionID`, `about`) 
 (3, 'Jonas Schmedtmann', 'dfhgfhfg', 'quangtrung@gmail.com', 3, NULL);
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `category`
+-- Chỉ mục cho bảng `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`),
@@ -336,7 +284,7 @@ ALTER TABLE `category`
   ADD UNIQUE KEY `name_UNIQUE` (`name`);
 
 --
--- Indexes for table `chapter`
+-- Chỉ mục cho bảng `chapter`
 --
 ALTER TABLE `chapter`
   ADD PRIMARY KEY (`id`),
@@ -344,7 +292,7 @@ ALTER TABLE `chapter`
   ADD UNIQUE KEY `name_UNIQUE` (`name`);
 
 --
--- Indexes for table `course`
+-- Chỉ mục cho bảng `course`
 --
 ALTER TABLE `course`
   ADD PRIMARY KEY (`id`),
@@ -353,52 +301,52 @@ ALTER TABLE `course`
   ADD KEY `idCategory_idx` (`idCategory`);
 
 --
--- Indexes for table `course_of_student`
+-- Chỉ mục cho bảng `course_of_student`
 --
 ALTER TABLE `course_of_student`
   ADD PRIMARY KEY (`studentID`,`courseID`);
 
 --
--- Indexes for table `course_of_teacher`
+-- Chỉ mục cho bảng `course_of_teacher`
 --
 ALTER TABLE `course_of_teacher`
   ADD PRIMARY KEY (`teacherID`,`courseID`);
 
 --
--- Indexes for table `history_lesson`
+-- Chỉ mục cho bảng `history_lesson`
 --
 ALTER TABLE `history_lesson`
   ADD PRIMARY KEY (`studentID`,`courseID`);
 
 --
--- Indexes for table `lesson`
+-- Chỉ mục cho bảng `lesson`
 --
 ALTER TABLE `lesson`
   ADD PRIMARY KEY (`id`,`chapterID`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
--- Indexes for table `permission`
+-- Chỉ mục cho bảng `permission`
 --
 ALTER TABLE `permission`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `idpermission_UNIQUE` (`id`);
 
 --
--- Indexes for table `rating`
+-- Chỉ mục cho bảng `rating`
 --
 ALTER TABLE `rating`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
--- Indexes for table `student_love_course`
+-- Chỉ mục cho bảng `student_love_course`
 --
 ALTER TABLE `student_love_course`
   ADD PRIMARY KEY (`studentID`,`courseID`);
 
 --
--- Indexes for table `user`
+-- Chỉ mục cho bảng `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
@@ -407,41 +355,41 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `email_UNIQUE` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `category`
+-- AUTO_INCREMENT cho bảng `category`
 --
 ALTER TABLE `category`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `chapter`
+-- AUTO_INCREMENT cho bảng `chapter`
 --
 ALTER TABLE `chapter`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `course`
+-- AUTO_INCREMENT cho bảng `course`
 --
 ALTER TABLE `course`
   MODIFY `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `lesson`
+-- AUTO_INCREMENT cho bảng `lesson`
 --
 ALTER TABLE `lesson`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `rating`
+-- AUTO_INCREMENT cho bảng `rating`
 --
 ALTER TABLE `rating`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
