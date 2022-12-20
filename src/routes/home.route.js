4d5682773bcbab87a33c8969f61541947a4861bb
Vylee <87347391+Vylee29@users.noreply.github.com>
2022-12-20 21:53:17 +0700

    update create course UI with db

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import HomeController from "../controllers/HomeController.js";
+import tCourseController from "../controllers/teacher/tCourseController.js";
 import ProfileController from "../controllers/user/ProfileController.js";
 
 const router = express.Router();
@@ -8,9 +9,7 @@ router.get("/", HomeController.index);
 
 router.get("/profile", ProfileController.index);
 
-router.get("/createCourse", (req, res) => {
-  res.render("createCourse");
-});
+router.get("/createCourse", tCourseController.index);
 // router.get("/courses/courseDetail", courseDetailController.index);
 // Routes
 router.get("/login", (req, res) => {
