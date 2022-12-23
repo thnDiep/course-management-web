1b1af9bbcd0710cee317c21e153639fda71328bc
thnDiep <thndiepkhtn@gmail.com>
2022-12-24 00:48:01 +0700

    update UI courses, search with DB & fix avt, title, label in course card

--- src/controllers/CourseController.js
+++ src/controllers/CourseController.js
@@ -1,28 +1,45 @@
 import courseModel from "../models/courseModel.js";
 import categoryModel from "../models/categoryModel.js";
-
+import userModel from "../models/userModel.js";
 class CourseController {
-  // [GET] /courses
+  // [GET] /courses?id=
   async index(req, res) {
     const id = parseInt(req.query.id) || 1;
     const category = await categoryModel.getById(id);
-    const linkCategories = [];
-
-    if (category.parentID !== null) {
-      const parentCategory = await categoryModel.getById(category.parentID);
-      linkCategories.push(parentCategory);
-    }
-    linkCategories.push(category);
-
     const courses = await courseModel.getByCategoryId(id);
-
     const allCategories = await categoryModel.getAllWithHierarchy(id); // use in category side bar
+
+    for (const course of courses) {
+      const avgRated = await courseModel.getAvgRate(course.id);
+      course.rated = (+avgRated).toFixed(1);
+
+      const numberRated = await courseModel.getCountFeedback(course.id);
+      course.numberRated = (+numberRated).toFixed(0);
+
+      const teacher = await userModel.getNameTeacher(course.id);
+      course.teacher = teacher;
+
+      const category = await categoryModel.getById(course.idCategory);
+      const linkCategories = [];
+
+      if (category.parentID !== null) {
+        const parentCategory = await categoryModel.getById(category.parentID);
+        linkCategories.push(parentCategory);
+      }
+      linkCategories.push(category);
+
+      course.linkCategories = linkCategories;
+
+      if (course.fee !== course.feeO) {
+        course.discount = true;
+      }
+    }
+
     res.render("courses", {
       category,
       allCategories,
       courses,
       isEmpty: courses.length === 0,
-      linkCategories,
     });
   }
 
--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,12 +1,24 @@
 import categoryModel from "../models/categoryModel.js";
 import courseModel from "../models/courseModel.js";
+import userModel from "../models/userModel.js";
+import homeModel from "../models/homeModel.js";
 class SearchController {
   // [GET] /search?q=
   async index(req, res) {
     const keyWord = parseInt(req.query.q);
     const courses = await courseModel.getAll(); // Function tìm kiếm
+    const [lastestList] = await homeModel.getLatest();
 
     for (const course of courses) {
+      const avgRated = await courseModel.getAvgRate(course.id);
+      course.rated = (+avgRated).toFixed(1);
+
+      const numberRated = await courseModel.getCountFeedback(course.id);
+      course.numberRated = (+numberRated).toFixed(0);
+
+      const teacher = await userModel.getNameTeacher(course.id);
+      course.teacher = teacher;
+
       const category = await categoryModel.getById(course.idCategory);
       const linkCategories = [];
 
@@ -17,6 +29,10 @@ class SearchController {
       linkCategories.push(category);
 
       course.linkCategories = linkCategories;
+
+      if (course.fee !== course.feeO) {
+        course.discount = true;
+      }
     }
 
     res.render("search", {
