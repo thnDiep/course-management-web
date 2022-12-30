9a66ab81b56f7975ce91db055320cd9a7c324292
Vylee <87347391+Vylee29@users.noreply.github.com>
2022-12-30 18:51:10 +0700

    create page learning

--- src/controllers/user/ProfileController.js
+++ src/controllers/user/ProfileController.js
@@ -1,22 +1,34 @@
 import profileUserModel from "../../models/profileUserModel.js";
 import AccountController from "../AccountController.js";
+import courseModel from "../../models/courseModel.js";
 class uProfileController {
   // GET categories list
   async index(req, res) {
-    // console.log("HIIIIIIIIIIIIIIIIIIII")
-    // console.log(req.session.authUser)
-    // console.log("BARRRRRRRRRRRRRRRRRRRRRR")
-    // console.log(res.locals.lcAuthUser.id)
-
     const [profiles] = await profileUserModel.getById(res.locals.lcAuthUser.id);
-    const lesson_learned = await profileUserModel.getLessonLearning(res.locals.lcAuthUser.id);
-    console.log("BARRRRRRRRRRRRRRRRRRRRRR")
-    console.log(lesson_learned)
-    const lesson_love = await profileUserModel.getWatchList(res.locals.lcAuthUser.id);
+    const lesson_learned = await profileUserModel.getLessonLearning(
+      res.locals.lcAuthUser.id
+    );
+    // console.log("BARRRRRRRRRRRRRRRRRRRRRR")
+    // console.log(lesson_learned)
+    const lesson_love = await profileUserModel.getWatchList(
+      res.locals.lcAuthUser.id
+    );
     res.render("profile", {
       profiles,
       lesson_learned,
       lesson_love,
+    });
+  }
+  async getLearning(req, res) {
+    const learning = await profileUserModel.getLessonLearning(
+      res.locals.lcAuthUser.id
+    );
+    for (let i = 0; i < learning.length; i++) {
+      const rated = await courseModel.getAvgRate(learning[i].id);
+      learning[i].rated = (+rated).toFixed(1);
+    }
+    res.render("learningCourses", {
+      learning,
     });
   }
 }
--- src/models/profileUserModel.js
+++ src/models/profileUserModel.js
@@ -9,7 +9,9 @@ export default {
   },
   async getLessonLearning(id) {
     const list = await db.raw(
-      `SELECT course.name as CourseName,user.name as TeacherName, course.image as Img FROM history_lesson,course_of_teacher,user,course,course_of_student WHERE history_lesson.courseID=course_of_teacher.courseID && history_lesson.courseID =course_of_student.courseID && history_lesson.studentID = course_of_student.studentID && course_of_teacher.teacherID=user.id && course_of_teacher.courseID=course.id && history_lesson.studentID= ?`,
+      `SELECT course.*,history_lesson.*,user.name as TeacherName
+      FROM history_lesson,course_of_teacher,user,course,course_of_student 
+      WHERE history_lesson.courseID=course_of_teacher.courseID && history_lesson.courseID =course_of_student.courseID && history_lesson.studentID = course_of_student.studentID && course_of_teacher.teacherID=user.id && course_of_teacher.courseID=course.id && history_lesson.studentID= ?`,
       id
     );
     return list[0];
