36bee3bcdabe027415a26809f25cee17c28c9338
manhtu227 <manhtu2272002@gmail.com>
2023-01-08 22:40:59 +0700

    fix footer and add link 404

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import HomeController from "../controllers/HomeController.js";
 import ProfileController from "../controllers/user/ProfileController.js";
-import accountRoute from "./account.route.js";
 import auth from "../middlewares/auth.mdw.js";
 import isUser from "../middlewares/isUser.mdw.js";
 
@@ -11,6 +10,7 @@ const router = express.Router();
 router.get("/profile", auth, isUser, ProfileController.index);
 // Edit + Update profile
 router.post("/profile", auth, isUser, ProfileController.updateProfile);
+router.get("/:slug", (req, res) => res.render("404"));
 
 router.get("/", HomeController.index);
 // router.get("/:slug", HomeController.index);
