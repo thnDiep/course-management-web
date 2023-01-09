77be9996941eae5014d9dab15dc80e203acad321
thnDiep <thndiepkhtn@gmail.com>
2023-01-10 03:05:41 +0700

    update courses, search, learning views

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,13 +4,9 @@ export default knex({
   client: "mysql2",
   connection: {
     host: "127.0.0.1",
-    port: 3306,
+    port: 3307,
     user: "root",
     password: "",
-<<<<<<< HEAD
-    database: "academy",
-=======
     database: "academydb",
->>>>>>> 840c5b7474c63ebacc77b17e973f462e7b3cef13
   },
 });
